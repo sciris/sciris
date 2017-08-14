98053a7d93e8b036fd3d5655d3c457bf4450269c
gchadder3 <gchadder3@gmail.com>
2017-08-14 16:57:51 -0500

    Created initial server startable from bin/win_server.cmd

---
+++ bin/win_server.cmd
@@ -0,0 +1,3 @@
+cd ../session_manager
+python _twisted_wsgi.py 8080
+cd ../bin
---
+++ session_manager/api.py
@@ -0,0 +1,36 @@
+"""
+api.py -- script for setting up a flask server
+    
+Last update: 8/14/17 (gchadder3)
+"""
+
+# Do the imports.
+# from pylab import figure, scatter, rand
+import model
+import mpld3
+import json
+from flask import Flask, request
+
+# Create the app
+app = Flask(__name__)
+
+# Define the API
+@app.route('/api', methods=['GET', 'POST'])
+def showGraph():
+    # Create the matplotlib graph from Python.
+    graphdata = model.makegraph()
+    
+    # Convert the figure to JSON.
+    graphjson = json.dumps(mpld3.fig_to_dict(graphdata)) 
+    
+    # Return the JSON representation of the Matplotlib figure.
+    return graphjson 
+    # Use this for the GET method.
+    #return 'I will give you info for: %s ' % request.args['value']
+
+    # Use this for the POST method.
+    #return 'I will give you info for: %s ' % request.get_json()['value']
+    
+
+if __name__ == "__main__":
+    app.run()
