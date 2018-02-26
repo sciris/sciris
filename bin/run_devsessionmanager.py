08e3d333f17bf71e08e0601f6b61dbd19e0532e7
cliffckerr <questionably@gmail.com>
2018-02-26 23:47:52 +0200

    hmm

--- bin/run_devsessionmanager.py
+++ bin/run_devsessionmanager.py
@@ -1,11 +1,9 @@
 # Ensure that the current folder is used, not the global defaults
 import sys
 import os
-scirisfolder = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.insert(0, scirisfolder) 
 
 # Load Optima
-from sessionmanager.api import app
+from sciris.api import app
 
 # Run the server
 app.run()
