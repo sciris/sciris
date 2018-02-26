6924fac80b6cf4020386773f5824f489ec74386d
cliffckerr <questionably@gmail.com>
2018-02-26 23:02:04 +0200

    oops

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from setuptools import setup, find_packages
 
-with open("version.py", "r") as f:
+with open("sciris/version.py", "r") as f:
     version_file = {}
     exec(f.read(), version_file)
     version = version_file["version"]
