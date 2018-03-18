8417d126b53cbaecd43a9e02e88583f44a14c68f
cliffckerr <questionably@gmail.com>
2018-03-18 14:20:38 +0100

    trying switch instead?

--- sciris/__init__.py
+++ sciris/__init__.py
@@ -3,14 +3,15 @@ import version
 string = 'Sciris v%s (%s) loaded' % (version.version, version.versiondate)
 try:
     import os
-    import matplotlib
+    import matplotlib.pyplot as plt
     if 'DISPLAY' in os.environ:
         if not os.environ['DISPLAY']:
-            matplotlib.use('agg')
+            plt.switch_backend('agg')
             print(string+' for server use')
         else:
-            print(string+' for local use')
+            print(string+' for local use (display=%s)' % os.environ['DISPLAY'])
     else:
+        plt.switch_backend('agg')
         print(string+'; display not recognized')
 except Exception as E:
 	print(string+'; could not set Matplotlib backend (%s), proceeding anyway...' % repr(E))
