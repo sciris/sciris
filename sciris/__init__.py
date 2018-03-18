c052e4f86ebd35e58d3978d5b4e0d983dafe5a2a
cliffckerr <questionably@gmail.com>
2018-03-18 13:54:02 +0100

    made init.py more robust, hopefully

--- sciris/__init__.py
+++ sciris/__init__.py
@@ -1,6 +1,9 @@
 # To allow graphs to be created without a DISPLAY variable -- WARNING, maybe a cleaner way of doing this?
 try:
-	import matplotlib
-	matplotlib.use('agg')
+    import os
+    import matplotlib
+    if 'DISPLAY' in os.environ:
+        if not os.environ['DISPLAY']:
+            matplotlib.use('agg')
 except:
 	print('Sciris initialization: could not set Matplotlib backend, proceeding anyway...')
