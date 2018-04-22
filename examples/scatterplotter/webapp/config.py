1734bf9ff34354eedfb2880a825817f1dea33cde
George Chadderdon <gchadder3@gmail.com>
2018-04-21 23:30:23 -0500

    Improved config.py so it doesn't depend on absolute path

--- examples/scatterplotter/webapp/config.py
+++ examples/scatterplotter/webapp/config.py
@@ -8,39 +8,35 @@
 # you use a relative path, it is interpreted as being with respect to the 
 # root path of the main repository.
 
-# Last update: 2018mar19
+# Last update: 2018apr21
 
 import os
 
 # A secret key value used by Python Flask.
 SECRET_KEY = 'Pick something unique for your site here'
 
-ROOT_DIR = os.pardir + os.sep  # go up one directory from /bin
+# Grab the directory up one level from where we are (/bin).
+ROOT_DIR = os.path.abspath(os.pardir) + os.sep
 
 # Directory containing the client code.
-#CLIENT_DIR = ROOT_DIR + 'vueinterface'
-CLIENT_DIR = 'C:\\GitRepos\\OptimaRepos\\sciris\\examples\\scatterplotter\\vueinterface'
+CLIENT_DIR = ROOT_DIR + 'vueinterface'
 
 # Directory containing the model code.  This is the primary Python code that 
 # the web app will rely on related to the actual model.
-#MODEL_DIR = ROOT_DIR + 'scirismodel'
-MODEL_DIR = 'C:\\GitRepos\\OptimaRepos\\sciris\\examples\\scatterplotter\\scirismodel'
+MODEL_DIR = ROOT_DIR + 'scirismodel'
 
 # Directory containing Python code specific to the web app.  sessionmanager 
 # (the directory this config file is in) contains general Python functionality 
 # that the web app directory code makes use of. 
-#WEBAPP_DIR = ROOT_DIR + 'webapp'
-WEBAPP_DIR = 'C:\\GitRepos\\OptimaRepos\\sciris\\examples\\scatterplotter\\webapp'
+WEBAPP_DIR = ROOT_DIR + 'webapp'
 
 # Directory where uploaded files will be saved, as well as files saved to be 
 # downloadable to user machines.
-#UPLOADS_DIR = ROOT_DIR + 'uploads'
-UPLOADS_DIR = 'C:\\GitRepos\\OptimaRepos\\sciris\\examples\\scatterplotter\\uploads'
+UPLOADS_DIR = ROOT_DIR + 'uploads'
 
 # Directory where files may be saved that the web app should be able to 
 # access.
-#FILESAVEROOT_DIR = ROOT_DIR + 'savedfiles'
-FILESAVEROOT_DIR = 'C:\\GitRepos\\OptimaRepos\\sciris\\examples\\scatterplotter\\savedfiles'
+FILESAVEROOT_DIR = ROOT_DIR + 'savedfiles'
 
 # URL for the Redis database that the web app will use to manage 
 # persistence.  Note that the /N/ number at the end should match the 
