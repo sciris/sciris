04b2f9fb71cfa21f87b469f80fb9b7a30c5d53a1
George Chadderdon <gchadder3@gmail.com>
2018-04-30 15:40:39 -0500

    Created a sciris2gc package to work in for Shiny-esque apps (used instead of the usual sciris package)

--- examples/helloneighbor/miniapi.py
+++ examples/helloneighbor/miniapi.py
@@ -5,12 +5,27 @@ Simple demonstration Flask webapp.
 # Imports
 from flask import Flask
 
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import mpld3
+#from mpld3 import plugins
+#
+#fig, ax = plt.subplots()
+#points = ax.scatter(np.random.rand(40), np.random.rand(40),
+#                    s=300, alpha=0.3)
+#
+#labels = ["Point {0}".format(i) for i in range(40)]
+#tooltip = plugins.PointLabelTooltip(points, labels)
+#
+#plugins.connect(fig, tooltip)
+
 # Create the app
 app = Flask(__name__) 
 
 # Define the API
 @app.route('/api', methods=['GET', 'POST'])
 def myPage():
+#    return mpld3.fig_to_html(fig)
     return '<h1>Hello, Flask!</h1>'
 
 # Run the webapp
---
+++ examples/helloneighbor/run_server.py
@@ -0,0 +1,6 @@
+"""
+Run the server using Twisted.
+"""
+
+import sciris2gc
+sciris2gc.server.run()
