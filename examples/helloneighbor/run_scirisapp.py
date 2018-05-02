d5823dfb011fff21b5c6cb169bc59f4145c3844e
George Chadderdon <gchadder3@gmail.com>
2018-05-02 13:15:04 -0500

    Added graph to simple use case

--- examples/helloneighbor/run_scirisapp.py
+++ examples/helloneighbor/run_scirisapp.py
@@ -1,9 +1,13 @@
 """
-Simple ScirisApp use case
+run_scirisapp.py -- Simple ScirisApp use case 
+    
+Last update: 5/1/18 (gchadder3)
 """
 
 # Imports
 from sciris2gc.scirisapp import ScirisApp
+import model # The actual Python model we want to incorporate
+import mpld3 # For plotting
 
 # Run the webapp
 if __name__ == "__main__":
@@ -13,12 +17,18 @@ if __name__ == "__main__":
     appEndpointHandler = theApp.flaskApp.route
     
     @appEndpointHandler('/')
-    def myPage():
+    def myRootPage():
         return '<h1>Hello, Flask!</h1>'
     
     @appEndpointHandler('/api')
-    def myPage2():
-        return '<h1>Look at that!  Two flask endpoints!</h1>' 
+    def myApiPage():
+        return '<h1>Look at that!  Two Flask endpoints!</h1>' 
+    
+    @appEndpointHandler('/graph')
+    def myGraphPage():
+        graphFig = model.makegraph()
+        graphHtml = mpld3.fig_to_html(graphFig)
+        return '<h1>My graph</h1>' + graphHtml
         
     # Run the Flask server in the app.
     theApp.runServer()
