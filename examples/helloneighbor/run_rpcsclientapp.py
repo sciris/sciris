bc9817963375193e4b78212139d50364a2fe0276
George Chadderdon <gchadder3@gmail.com>
2018-05-08 21:32:48 -0500

    Started development of RPCs in new framework

---
+++ examples/helloneighbor/run_rpcsclientapp.py
@@ -0,0 +1,38 @@
+"""
+run_rpcsclientapp.py -- Simple ScirisApp use case 
+    
+Last update: 5/8/18 (gchadder3)
+"""
+
+# Imports
+from sciris2gc.scirisapp import ScirisApp
+import model # The actual Python model we want to incorporate
+import mpld3 # For plotting
+import json # For converting to HTML
+import numpy as np
+
+# Create the ScirisApp object.
+app = ScirisApp(client_path='vueclient_rpcs')
+
+# Create a callback rendering function at /.
+@app.define_endpoint_callback('/')
+def my_root_page():
+    return '<h1>Hello, Flask!</h1>'
+
+
+
+# stuff to take out...
+    
+from sciris2gc.scirisapp import json_sanitize_result
+ 
+# Create a callback rendering function at /.
+@app.define_endpoint_callback('/rpcs', methods=['POST'])
+def rpc():
+    new_graph = model.makegraph()
+    graph_dict = mpld3.fig_to_dict(new_graph)
+    graph_json = json.dumps(json_sanitize_result(graph_dict)) # Convert to JSON
+    return graph_json # Return the JSON representation of the Matplotlib figure
+
+
+# Run the client page with Flask and a Twisted server.
+app.run_server()
--- sciris2gc/scirisapp.py
+++ sciris2gc/scirisapp.py
@@ -1,12 +1,13 @@
 """
 scirisapp.py -- classes for Sciris (Flask-based) apps 
     
-Last update: 5/7/18 (gchadder3)
+Last update: 5/8/18 (gchadder3)
 """
 
 # Imports
 from flask import Flask, request
 import sys
+import numpy as np
 from twisted.internet import reactor
 from twisted.internet.endpoints import serverFromString
 from twisted.logger import globalLogBeginner, FileLogObserver, formatEvent
@@ -156,4 +157,55 @@ def run_twisted(port=8080, flask_app=None, client_path=None):
     endpoint.listen(site)
 
     # Start the reactor.
-    reactor.run()        
+    reactor.run()  
+    
+    
+def json_sanitize_result(theResult):
+    """
+    This is the main conversion function for Python data-structures into
+    JSON-compatible data structures.
+    Use this as much as possible to guard against data corruption!
+    Args:
+        theResult: almost any kind of data structure that is a combination
+            of list, numpy.ndarray, etc.
+    Returns:
+        A converted dict/list/value that should be JSON compatible
+    """
+
+    if isinstance(theResult, list) or isinstance(theResult, tuple):
+        return [json_sanitize_result(p) for p in list(theResult)]
+    
+    if isinstance(theResult, np.ndarray):
+        if theResult.shape: # Handle most cases, incluing e.g. array([5])
+            return [json_sanitize_result(p) for p in list(theResult)]
+        else: # Handle the special case of e.g. array(5)
+            return [json_sanitize_result(p) for p in list(np.array([theResult]))]
+
+    if isinstance(theResult, dict):
+        return {str(k): json_sanitize_result(v) for k, v in theResult.items()}
+
+    if isinstance(theResult, np.bool_):
+        return bool(theResult)
+
+    if isinstance(theResult, float):
+        if np.isnan(theResult):
+            return None
+
+    if isinstance(theResult, np.float64):
+        if np.isnan(theResult):
+            return None
+        else:
+            return float(theResult)
+
+    if isinstance(theResult, unicode):
+        return theResult
+#        return str(theResult)  # original line  (watch to make sure the 
+#                                                 new line doesn't break things)
+    
+    if isinstance(theResult, set):
+        return list(theResult)
+
+#    if isinstance(theResult, UUID):
+#        return str(theResult)
+
+    return theResult    
