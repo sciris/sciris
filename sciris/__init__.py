554c75e1d2a55aba7c06a295292d4f5629ee0390
cliffckerr <questionably@gmail.com>
2018-05-18 04:50:20 -0500

    trying again with the display variable

--- sciris/__init__.py
+++ sciris/__init__.py
@@ -2,22 +2,9 @@ from . import utils # Import utilities -- these will always be required
 from . import core
 from .utils import odict # Import odict class
 from .utils import dataframe # Import dataframe class
+import os as _os
+import matplotlib as _mpl
 
-#from . import version
-#print('Sciris v%s (%s) loaded' % (version.version, version.versiondate))
-
-## To allow graphs to be created without a DISPLAY variable -- WARNING, maybe a cleaner way of doing this?
-#try:
-#    import os
-#    import matplotlib.pyplot as plt
-#    if 'DISPLAY' in os.environ:
-#        if not os.environ['DISPLAY']:
-#            plt.switch_backend('agg')
-#            print(string+' for server use')
-#        else:
-#            print(string+' for local use (display=%s)' % os.environ['DISPLAY'])
-#    else:
-#        plt.switch_backend('agg')
-#        print(string+'; display not recognized')
-#except Exception as E:
-#	print(string+'; could not set Matplotlib backend (%s), proceeding anyway...' % repr(E))
+if _os.environ.get('DISPLAY','') == '':
+    print('Sciris: no display found, using non-interactive Agg backend')
+    _mpl.use('Agg')
