788fc32bfc11913a58a1dc08c8ecf7a57e282373
cliffckerr <questionably@gmail.com>
2018-09-11 21:21:53 +1000

    still required

--- setup-web.py
+++ setup-web.py
@@ -44,5 +44,6 @@ setup(
         'celery>=4.2', # Task manager
         'twisted>=18.4.0', # Server
         'service_identity', # Identity manager for Celery (not installed with Celery though)
+        'pyasn1', # Required for service_identity (but not listed as a dependency!)
     ],
 )
