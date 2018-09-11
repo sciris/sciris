367b1de8063fd7d59e301f9f9b861519198751d7
cliffckerr <questionably@gmail.com>
2018-09-12 00:52:39 +1000

    increment version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,4 +1,4 @@
-version       = '0.7.4'
-versiondate   = '2018-09-08'
+version       = '0.7.5'
+versiondate   = '2018-09-11'
 scirislicense = 'Sciris %s (%s) -- (c) 2018 by the Optima Consortium (info@sciris.org)' % (version, versiondate)
 
