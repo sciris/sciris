a5e7ba5c73b136856582654c57c2867e16954224
cliffckerr <questionably@gmail.com>
2018-09-24 01:06:02 +1000

    the votes are in, sort of

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -2,4 +2,4 @@ __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__      = '0.9.2'
 __versiondate__  = '2018-09-23'
-__license__      = 'Sciris %s (%s) -- (c) ScirisOrg (info@sciris.org)' % (__version__, __versiondate__)
+__license__      = 'Sciris %s (%s) -- (c) Sciris Community (info@sciris.org)' % (__version__, __versiondate__)
