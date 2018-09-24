264f4f2e686ca67793ed342f8aa1c3e0ac50d76b
cliffckerr <questionably@gmail.com>
2018-09-24 11:28:56 +1000

    new license

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -2,4 +2,4 @@ __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__      = '0.9.3'
 __versiondate__  = '2018-09-23'
-__license__      = 'Sciris %s (%s) -- (c) Sciris Community (info@sciris.org)' % (__version__, __versiondate__)
+__license__      = 'Sciris %s (%s) -- (c) Sciris Development Team (info@sciris.org)' % (__version__, __versiondate__)
