75b58bc7291cfcce2ffe3c89a1cdc28a11f92a02
cliffckerr <questionably@gmail.com>
2018-10-20 12:25:32 +1100

    not sure why its not working

--- examples/py-shiny/app.py
+++ examples/py-shiny/app.py
@@ -4,31 +4,43 @@ import sciris as sc
 import scirisweb as sw
 import datetime
 
-runserver = False # Choose to run in the frontend or backend
+runserver = True # Choose to run in the frontend or backend
+
+# Get the data
+def loaddata():
+    dataurl = 'https://raw.githubusercontent.com/rstudio/shiny-examples/master/120-goog-index/data/trend_data.csv'
+    rawdata = sc.wget(dataurl).splitlines()
+    data = []
+    for r,rawline in enumerate(rawdata):
+        line = rawline.split(',')
+        if r==0: # Read header
+            cols = line
+        else: # Read data
+            tag = line[0]
+            date = datetime.datetime.strptime(line[1], '%Y-%m-%dT%I:%M:%fZ')
+            yearnum = date.year + (date.month-1)/12. + (date.day-1)/365.
+            value = float(line[2]) if r>0 else line[2]
+            data.append([tag, yearnum, value])
+    df = sc.dataframe(cols=cols, data=data)
+    return df
 
 # Create the app
 app = sw.ScirisApp(__name__, name="PyShiny")
-
-# Get the data
-dataurl = 'https://raw.githubusercontent.com/rstudio/shiny-examples/master/120-goog-index/data/trend_data.csv'
-rawdata = sc.wget(dataurl).splitlines()
-data = []
-for r,rawline in enumerate(rawdata):
-    line = rawline.split(',')
-    if r==0: # Read header
-        cols = line
-    else: # Read data
-        tag = line[0]
-        date = datetime.datetime.strptime(line[1], '%Y-%m-%dT%I:%M:%fZ')
-        yearnum = date.year + (date.month-1)/12 + (date.day-1)/365
-        value = float(line[2]) if r>0 else line[2]
-        data.append([tag, yearnum, value])
-df = sc.dataframe(cols=cols, data=data)
-
+df = loaddata()
 
 # Define the API
+
+@app.route('/getoptions')
+def getoptions():
+    output = sc.sanitizejson(list(set(df['type'])), tostring=True)
+    return output
+
 @app.route('/plotdata')
 def plotdata(datatype=None, startdate=None, enddate=None):
+    
+    if datatype  is None: datatype  = df['type'][0]
+    if startdate is None: startdate = -pl.inf
+    if enddate   is None: enddate   = pl.inf
     
     # Make graph
     fig = pl.figure()
@@ -36,8 +48,9 @@ def plotdata(datatype=None, startdate=None, enddate=None):
     thesedata = df.findrows(key=datatype, col='type')
     years = thesedata['date']
     vals = thesedata['close']
-    validinds = sc.findinds(years>=float(startdate) and years<=float(enddate))
+    validinds = sc.findinds(pl.logical_and(years>=float(startdate), years<=float(enddate)))
     ax.plot(years[validinds], vals[validinds])
+    print(years)
     
     # Convert to FE
     graphjson = sw.mpld3ify(fig)  # Convert to dict
@@ -47,4 +60,4 @@ def plotdata(datatype=None, startdate=None, enddate=None):
 if __name__ == "__main__" and runserver:
     app.run()
 else:
-    showgraph()
+    plotdata()
