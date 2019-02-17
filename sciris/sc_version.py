580b600dd6bd2086345af66840cb33869e8ac92c
cliffckerr <questionably@gmail.com>
2019-02-18 10:36:58 +1100

    add uuid to list of accepted types

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.13.3'
-__versiondate__  = '2019-02-17'
+__version__      = '0.13.4'
+__versiondate__  = '2019-02-18'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
