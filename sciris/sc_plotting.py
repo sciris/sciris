9cbe0bbeb86d32d45cb91878ee03489d720d193c
cliffckerr <questionably@gmail.com>
2019-08-21 23:03:24 -0700

    updated examples

--- sciris/sc_plotting.py
+++ sciris/sc_plotting.py
@@ -947,28 +947,38 @@ def savemovie(frames, filename=None, fps=None, quality=None, dpi=None, writer=No
         A Matplotlib animation object
     
     
-    Example
-    -------
+    Examples
+    --------
     import pylab as pl
     import sciris as sc
+    
+    # Simple example (takes ~5 s)
+    frames = [pl.plot(pl.cumsum(pl.randn(100))) for i in range(20)] # Create frames
+    sc.savemovie(frames, 'dancing_lines.gif') # Save movie as medium-quality gif
+    
+    # Complicated example (takes ~15 s)
     nframes = 100 # Set the number of frames
     ndots = 100 # Set the number of dots
     axislim = 5*pl.sqrt(nframes) # Pick axis limits
     dots = pl.zeros((ndots, 2)) # Initialize the dots
     frames = [] # Initialize the frames
-    old_dots = sc.dcp(dots)
+    old_dots = sc.dcp(dots) # Copy the dots we just made
+    fig = pl.figure(figsize=(10,8)) # Create a new figure
     for i in range(nframes): # Loop over the frames
         dots += pl.randn(ndots, 2) # Move the dots randomly
         color = pl.norm(dots, axis=1) # Set the dot color
         old = pl.array(old_dots) # Turn into an array
         plot1 = pl.scatter(old[:,0], old[:,1], c='k') # Plot old dots in black
-        plot2 = pl.scatter(dots[:,0], dots[:,1], c=color) # Note: Frames will be combined here, but separate in the animation
-        pl.xlim((-axislim, axislim)) # Set the axis limits
-        pl.ylim((-axislim, axislim))
-        pl.title('Fleeing dots') # Note that unfortunately dynamic titles can't be 
-        frames.append((plot1, plot2)) # Store updated artists
+        plot2 = pl.scatter(dots[:,0], dots[:,1], c=color) # Note: Frames will be separate in the animation
+        pl.xlim((-axislim, axislim)) # Set x-axis limits
+        pl.ylim((-axislim, axislim)) # Set y-axis limits
+        titlekwargs = {'transform':pl.gca().transAxes, 'horizontalalignment':'center'} # Set the "title" properties
+        title = pl.text(0.5, 1.05, f'Iteration {i+1}/{nframes}', **titlekwargs) # Unfortunately pl.title() can't be dynamically updated
+        pl.xlabel('Latitude') # But static labels are fine
+        pl.ylabel('Longitude') # Ditto
+        frames.append((plot1, plot2, title)) # Store updated artists
         old_dots = pl.vstack([old_dots, dots]) # Store the new dots as old dots
-    sc.savemovie(frames, 'fleeing_dots.mp4') # Actually save the movie!
+    sc.savemovie(frames, 'fleeing_dots.mp4', fps=20, quality='high') # Save movie as a high-quality mp4
     
     Version: 2019aug21
     '''
@@ -978,8 +988,8 @@ def savemovie(frames, filename=None, fps=None, quality=None, dpi=None, writer=No
     if not isinstance(frames, list):
         raise Exception(f'savemovie(): Argument "frames" must be a list, not "{type(frames)}"')
     for f in range(len(frames)):
-        if not isinstance(frames[f], tuple):
-            frames[f] = (frames[f],) # Stupidly, this must be a tuple to work with ArtistAnimation
+        if not ut.isiterable(frames[f]):
+            frames[f] = (frames[f],) # This must be either a tuple or a list to work with ArtistAnimation
     
     # Try to get the figure from the frames, else use the current one
     try:    fig = frames[0][0].get_figure()
@@ -1000,7 +1010,7 @@ def savemovie(frames, filename=None, fps=None, quality=None, dpi=None, writer=No
     
     # Handle dpi/quality
     if dpi is None and quality is None:
-        quality = 'high' # Make it high quailty by default
+        quality = 'medium' # Make it medium quailty by default
     if isinstance(dpi, str):
         quality = dpi # Interpret dpi arg as a quality command
         dpi = None
@@ -1015,14 +1025,14 @@ def savemovie(frames, filename=None, fps=None, quality=None, dpi=None, writer=No
     # Optionally print progress
     if verbose:
         start = ut.tic()
-        print(f'Saving {len(frames)} frames at {fps} fps at {dpi} dpi to "{filename}" using {writer}...')
+        print(f'Saving {len(frames)} frames at {fps} fps and {dpi} dpi to "{filename}" using {writer}...')
     
     # Actually create the animation -- warning, no way to not actually have it render!
     anim = animation.ArtistAnimation(fig, frames, interval=interval, repeat_delay=repeat_delay, repeat=repeat, blit=blit)
     anim.save(filename, writer=writer, fps=fps, dpi=dpi, bitrate=bitrate, **kwargs)
 
     if verbose:
-        print(f'Movie saved to "{filename}"')
+        print(f'Done; movie saved to "{filename}"')
         try: # Not essential, so don't try too hard if this doesn't work
             filesize = os.path.getsize(filename)
             if filesize<1e6: print('File size: %0.0f KB' % (filesize/1e3))
