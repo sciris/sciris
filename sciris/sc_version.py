7bc5003f8be2e8552c31b12edb542f5daa1ec715
cliffckerr <questionably@gmail.com>
2019-08-22 16:33:40 -0700

    added more colormaps

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.14.2'
-__versiondate__  = '2019-08-21'
+__version__      = '0.14.3'
+__versiondate__  = '2019-08-22'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
