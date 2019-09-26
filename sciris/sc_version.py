0cf13235007cebb17576d017fe05d121cec559bb
cliffckerr <questionably@gmail.com>
2019-09-26 13:44:02 -0700

    increment version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.14.8'
-__versiondate__  = '2019-09-24'
+__version__      = '0.14.9'
+__versiondate__  = '2019-09-26'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
