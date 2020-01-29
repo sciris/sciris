d0765e717b1a75942411668caf7494afdc7fb460
cliffckerr <questionably@gmail.com>
2020-01-28 19:33:25 -0800

    fix saveobj and update version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.15.3'
-__versiondate__  = '2019-12-15'
+__version__      = '0.15.4'
+__versiondate__  = '2020-01-28'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
