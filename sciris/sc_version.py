da1a4e697c25022883027ab5eb97d6e763477ff3
cliffckerr <questionably@gmail.com>
2020-03-15 00:35:57 -0700

    update version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__      = '0.15.8'
-__versiondate__  = '2020-03-10'
+__versiondate__  = '2020-03-14'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
