ff4e00ed0eff7f10dbf9ba09fa80855fa29a974e
cliffckerr <questionably@gmail.com>
2020-03-20 01:07:13 -0700

    allow dict input to dataframe

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.15.8'
-__versiondate__  = '2020-03-14'
+__version__      = '0.15.9'
+__versiondate__  = '2020-03-19'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
