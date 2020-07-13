c8ef43cb9fb30e8a2949f9d278033dcc569ca454
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-07-13 08:21:13 +0800

    Add search function

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -1,5 +1,5 @@
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '0.17.1'
+__version__      = '0.17.2'
 __versiondate__  = '2020-07-07'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
