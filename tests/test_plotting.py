96ab8a2aa9be65cac7f6c36700a6aa267078aab5
cliffckerr <questionably@gmail.com>
2020-11-29 20:37:03 -0800

    initial plotting refactor

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -6,66 +6,100 @@ import pylab as pl
 import sciris as sc
 
 
-if 'doplot' not in locals(): doplot = True
+if 'doplot' not in locals(): doplot = False
 
-def test_plotting():
 
-    torun = [
-    'hex2rgb',
-    'arraycolors',
-    'gridcolors',
-    'surf3d',
-    'bar3d',
-    ]
+def test_colors(doplot):
+    sc.heading('Testing colors')
+    o = sc.objdict()
 
-    if 'hex2rgb' in torun:
-        c1 = sc.hex2rgb('#fff')
-        c2 = sc.hex2rgb('fabcd8')
-        print(c1)
-        print(c2)
+    print('Testing hex')
+    o.c1 = sc.hex2rgb('#fff')
+    o.c2 = sc.hex2rgb('fabcd8')
+    print(o.c1)
+    print(o.c2)
 
-    if 'arraycolors' in torun:
-        n = 1000
-        ncols = 5
-        arr = pl.rand(n,ncols)
+    print('Testing arraycolors')
+    n = 1000
+    ncols = 5
+    arr = pl.rand(n,ncols)
+    for c in range(ncols):
+        arr[:,c] += c
+    x = pl.rand(n)
+    y = pl.rand(n)
+    colors = sc.arraycolors(arr)
+    if doplot:
+        pl.figure(figsize=(20,16))
         for c in range(ncols):
-            arr[:,c] += c
-        x = pl.rand(n)
-        y = pl.rand(n)
-        colors = sc.arraycolors(arr)
-        if doplot:
-            pl.figure(figsize=(20,16))
-            for c in range(ncols):
-                pl.scatter(x+c, y, s=50, c=colors[:,c])
+            pl.scatter(x+c, y, s=50, c=colors[:,c])
+    o.arraycolors = colors
 
-    if 'gridcolors' in torun:
-        colors_a = sc.gridcolors(ncolors=8,  demo=doplot)
-        colors_b = sc.gridcolors(ncolors=18, demo=doplot)
-        colors_c = sc.gridcolors(ncolors=28, demo=doplot)
-        print('\n8 colors:', colors_a)
-        print('\n18 colors:', colors_b)
-        print('\n28 colors:', colors_c)
+    print('Testing gridcolors')
+    colors_a = sc.gridcolors(ncolors=8,  demo=doplot)
+    colors_b = sc.gridcolors(ncolors=18, demo=doplot)
+    colors_c = sc.gridcolors(ncolors=28, demo=doplot)
+    print('\n8 colors:', colors_a)
+    print('\n18 colors:', colors_b)
+    print('\n28 colors:', colors_c)
+    o.gridcolors = colors_a
 
-    if 'surf3d' in torun:
-        data = pl.randn(50,50)
-        smoothdata = sc.smooth(data,20)
-        if doplot:
-            sc.surf3d(smoothdata)
+    return o
 
-    if 'bar3d' in torun:
-        data = pl.rand(20,20)
-        smoothdata = sc.smooth(data)
-        if doplot:
-            sc.bar3d(smoothdata)
 
-    return pl.gcf()
+def test_colormaps(doplot):
+    sc.heading('Testing colormaps')
+    o = sc.objdict()
+    return o
+
+
+def test_3d(doplot):
+    sc.heading('Testing 3D')
+    o = sc.objdict()
+
+    print('Testing surf3d')
+    data = pl.randn(50,50)
+    smoothdata = sc.smooth(data,20)
+    if doplot:
+        sc.surf3d(smoothdata)
+
+    print('Testing bar3d')
+    data = pl.rand(20,20)
+    smoothdata = sc.smooth(data)
+    if doplot:
+        sc.bar3d(smoothdata)
+
+    return o
+
+
+def test_other(doplot):
+    sc.heading('Testing other')
+    o = sc.objdict()
+    return o
+
+
+def test_saving(doplot):
+    sc.heading('Testing saving')
+    o = sc.objdict()
+    return o
+
+
+
 
 
 #%% Run as a script
 if __name__ == '__main__':
     sc.tic()
 
-    fig = test_plotting()
+    doplot = True
+
+    colors    = test_colors(doplot)
+    colormaps = test_colormaps(doplot)
+    threed    = test_3d(doplot)
+    other     = test_other(doplot)
+    saved     = test_saving(doplot)
+
+    if doplot:
+        pl.show()
 
     sc.toc()
     print('Done.')
