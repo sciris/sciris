2c8525ba21969ebed772b915a26e83c83c212d63
cliffckerr <questionably@gmail.com>
2021-03-31 13:09:15 -0700

    update version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '1.1.1'
-__versiondate__  = '2021-03-17'
+__version__      = '1.1.2'
+__versiondate__  = '2021-03-31'
 __license__      = 'Sciris %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
