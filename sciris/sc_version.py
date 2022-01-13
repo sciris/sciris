d446ac04ea71283998bed167e21fd1ff646f69b3
cliffckerr <questionably@gmail.com>
2022-01-13 18:36:48 -0500

    update version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__      = '1.3.2'
-__versiondate__  = '2022-01-14'
+__versiondate__  = '2022-01-13'
 __license__      = f'Sciris {__version__} ({__versiondate__}) – © 2022 by Sciris.org'
