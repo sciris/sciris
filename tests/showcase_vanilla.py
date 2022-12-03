e4bd112eb0dff5de9cfb064699b3300bc6433e7e
cliffckerr <questionably@gmail.com>
2022-12-02 23:32:24 -0500

    temporary PR for screenshot

--- tests/showcase_vanilla.py
+++ tests/showcase_vanilla.py
@@ -1,11 +1,6 @@
 # Imports
 import numpy as np
-import time
-import multiprocessing as mp
-import pickle
-import gzip
-import matplotlib.pyplot as pl
-from mpl_toolkits.mplot3d import Axes3D # Unused but must be imported
+import sciris as sc
 
 # Set parameters and define random wave generator
 xmin = 0
@@ -21,41 +16,23 @@ def randwave(std):
     return a + b*std
 
 # Start timing
-start = time.time()
+sc.tic()
 
 # Create object in parallel
-multipool = mp.Pool(processes=mp.cpu_count())
-output = multipool.map(randwave, noisevals)
-multipool.close()
-multipool.join()
+output = sc.parallelize(randwave, noisevals)
 
 # Save to files
-filenames = []
+filenames = sc.autolist()
 for n,noiseval in enumerate(noisevals):
     filename = f'noise{noiseval:0.1f}.obj'
-    with gzip.GzipFile(filename, 'wb') as fileobj:
-        fileobj.write(pickle.dumps(output[n]))
-    filenames.append(filename)
+    sc.saveobj(filename, output[n])
+    filenames += filename
 
 # Create dict from files
-data = {}
-for filename in filenames:
-    with gzip.GzipFile(filename) as fileobj:
-        filestring = fileobj.read()
-        data[filename] = pickle.loads(filestring)
+data = sc.odict({filename:sc.loadobj(filename) for filename in filenames})
 
 # Create 3D plot
-data_array = np.array([data[filename] for filename in filenames])
-fig = pl.figure()
-ax = fig.gca(projection='3d')
-ax.view_init(elev=45, azim=30)
-ny,nx = np.array(data_array).shape
-x = np.arange(nx)
-y = np.arange(ny)
-X, Y = np.meshgrid(x, y)
-surf = ax.plot_surface(X, Y, data_array, cmap='viridis')
-fig.colorbar(surf)
+sc.surf3d(data[:])
 
 # Print elapsed time
-elapsed = time.time() - start
-print(f'Elapsed time: {elapsed:0.1f} s')
+sc.toc()
