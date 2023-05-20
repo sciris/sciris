ef6f2761267b8a2b256b2d2b027f90bcea62a98b
Paula Sanz-Leon <pmsl.academic@gmail.com>
2023-05-20 16:40:46 +1000

    Adapt to more traditional description, and parallelise sampling

--- sciris/sc_alias.py
+++ sciris/sc_alias.py
@@ -19,19 +19,20 @@ D. Knuth. The Art of Computer Programming, Vol 2: Seminumerical Algorithms, sect
 import numpy.random as npr
 import numpy as np
 from . import sc_utils as scu
+from . import sc_math as scm
+from . import sc_parallel as scp
 
 __all__ = ['alias_sampler']
 
 
-def sample(n, q, J, r1, r2):
-    res = np.zeros(n, dtype=np.int32)
-    lj = len(J)
-    for i in range(n):
-        kk = int(np.floor(r1[i] * lj))
-        if r2[i] < q[kk]:
-            res[i] = kk
-        else:
-            res[i] = J[kk]
+def sample(r1, r2, probs_table=None, alias_table=None):
+    res = scu.toarray(0, dtype=np.int32)
+    lj = len(alias_table)
+    kk = int(np.floor(r1 * lj))
+    if r2 < probs_table[kk]:
+        res[0] = kk
+    else:
+        res[0] = alias_table[kk]
     return res
 
 
@@ -54,13 +55,15 @@ class alias_sampler:
 
     """
 
-    def __init__(self, probs, vals=None, randseed=None, verbose=False):
+    def __init__(self, probs, vals=None, randseed=None, verbose=False, parallel=True):
         self.probs = scu.toarray(probs)
         self.vals = scu.toarray(vals)  # If vals is None, then self.vals is an empty array
         self.rng = np.random.default_rng(randseed)  # Construct a new Generator
-        self.K = K = len(probs)
-        self.q = q = np.zeros(K)
-        self.J = J = np.zeros(K, dtype=np.int32)
+        self.n = n = len(probs)
+        # Initialise tables
+        self.probs_table = probs_table = n * self.probs
+        self.alias_table = alias_table = np.empty((n,), dtype=np.int32)
+        self.parallel = parallel
 
         # Do some checks if both probs and vals are given
         if vals is not None:
@@ -72,22 +75,49 @@ class alias_sampler:
         # Run checks on probs
         self._check_probs(verbose)
 
-        smaller, larger = [], []
-        for kk, prob in enumerate(probs):
-            q[kk] = K * prob
-            if q[kk] < 1.0:  # underfull group
-                smaller.append(kk)
-            else:
-                larger.append(kk)
+        # Divide the table entries into three categories,
+        overfull = probs_table > 1.0
+        underfull = probs_table < 1.0
+        exactly_full = probs_table == 1.0
 
-        while len(smaller) > 0 and len(larger) > 0:
-            small, large = smaller.pop(), larger.pop()
-            J[small] = large
-            q[large] = q[large] - (1.0 - q[small])
-            if q[large] < 1.0:
-                smaller.append(large)
+        # Alias table K, initialise exactly full entries
+        alias_table[exactly_full] = scm.findinds(exactly_full)
+
+        # As long as not all prob table entries are exactly full, repeat the following steps:
+        while any(underfull) and any(overfull):
+            # Find True indices
+            uf = scm.findinds(underfull)
+            of = scm.findinds(overfull)
+
+            # Arbitrarily choose an overfull entry (i) and an underfull entry (j)
+            uf_j = uf(self.rng.choice(uf, 1))
+            of_i = of(self.rng.choice(of, 1))
+
+            # Allocate the unused space in entry j to outcome i, by setting Kj = i.
+            alias_table[uf_j] = of_i
+
+            # Remove the allocated space from entry i by changing
+            probs_table[of_i] = probs_table[of_i] - (1.0 - probs_table[uf_j])
+
+            # Entry j is now exactly full, so update the corresponding category arrays
+            underfull[uf_j] = False
+            exactly_full[uf_j] = True
+
+            # Assign entry i to the appropriate category based on the new value of Ui.
+            overfull[of_i] = False
+            if probs_table[of_i] > 1.0:
+                overfull[of_i] = True
+            elif probs_table[of_i] < 1.0:
+                underfull[of_i] = True
             else:
-                larger.append(large)
+                exactly_full[of_i] = True
+
+        # Update tables
+        self.probs_table = probs_table
+        self.alias_table = alias_table
+
+        # Create a generato
+        self.rng = np.random.default_rng(randseed)
 
     def _check_probs(self, verbose):
         """
@@ -117,14 +147,14 @@ class alias_sampler:
                 errormsg = f"Warning! Probabilities didn't add up to 1. Normalising by prob.sum(): {prob_sum}"
                 print(errormsg)
 
-    def draw_one(self):
-        K, q, J = self.K, self.q, self.J
-        kk = int(np.floor(npr.rand() * len(J)))
-        if npr.rand() < q[kk]:
-            return kk
+    def draw(self, n):
+        r1, r2 = self.rng.rand(n), self.rng.rand(n)
+        if self.parallel:
+            res = scp.parallelize(sample, iterarg=[r1, r2],
+                                          kwargs={'probs_table': self.probs_table,
+                                                  'alias_table': self.alias_table})
         else:
-            return J[kk]
-
-    def draw_n(self, n):
-        r1, r2 = npr.rand(n), npr.rand(n)
-        return sample(n, self.q, self.J, r1, r2)
+            res = np.zeros(n)
+            for i in range(n):
+                res[i] = sample(r1[i], r2[i], probs_table=self.probs_table, alias_table=self.alias_table)
+        return res
