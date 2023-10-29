db4bb127b897703534226e92a308f00f9bd290d3
cliffckerr <questionably@gmail.com>
2023-10-29 08:16:18 -0400

    updated changelog

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__      = '3.1.1'
-__versiondate__  = '2023-10-25'
+__versiondate__  = '2023-10-29'
 __license__      = f'Sciris {__version__} ({__versiondate__}) – © 2014-2023 by the Sciris Development Team'
