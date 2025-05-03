414809c6a491002e415b3faceb48e9822158a0b0
cliffckerr <questionably@gmail.com>
2025-05-03 17:04:13 -0400

    update version

--- sciris/sc_version.py
+++ sciris/sc_version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__      = '3.2.1'
-__versiondate__  = '2025-04-19'
+__version__      = '3.2.2'
+__versiondate__  = '2025-05-05'
 __license__      = f'Sciris {__version__} ({__versiondate__}) – © 2014-2025 by the Sciris Development Team'
