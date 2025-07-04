3dab4ebaa3bc1903e97f18b42b9e414467ddd88c
cliffckerr <questionably@gmail.com>
2025-07-04 19:53:13 -0400

    working on test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -4,8 +4,9 @@ Test color and plotting functions -- warning, opens up many windows!
 
 import os
 import numpy as np
-import pylab as pl
+import pandas as pd
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import sciris as sc
 import pytest
 
@@ -24,44 +25,44 @@ def test_3d(doplot=doplot):
     o.fig = sc.fig3d(num='Blank 3D')
 
     print('Testing surf3d')
-    data = pl.randn(50,50)
+    data = np.random.randn(50,50)
     smoothdata = sc.smooth(data,20)
     sc.surf3d(smoothdata, figkwargs=dict(num='surf3d'))
 
     print('Testing bar3d')
-    data = pl.rand(20,20)
+    data = np.random.rand(20,20)
     smoothdata = sc.smooth(data)
     sc.bar3d(smoothdata, figkwargs=dict(num='bar3d'))
-    
+
     print('Testing scatter3d')
     sc.scatter3d(smoothdata, figkwargs=dict(num='scatter3d'))
 
     if not doplot:
-        pl.close('all')
+        plt.close('all')
 
     return o
 
 
 def test_stackedbar(doplot=doplot):
     sc.heading('Testing stacked bar')
-    
-    pl.figure(num='Stacked bar')
-    
+
+    plt.figure(num='Stacked bar')
+
     bmt = ['bottom','middle','top']
     lmr = ['left','middle','right']
-    
-    pl.subplot(1,2,1)
-    values = pl.rand(3,4)
-    artists = sc.stackedbar(values, labels=bmt)
-    pl.legend()
 
-    pl.subplot(1,2,2)
+    plt.subplot(1,2,1)
+    values = np.random.rand(3,4)
+    artists = sc.stackedbar(values, labels=bmt)
+    plt.legend()
+
+    plt.subplot(1,2,2)
     artists = sc.stackedbar(np.cumsum(values, axis=0).T, labels=lmr, is_cum=True, transpose=True, barh=True)
-    pl.legend()
-    
+    plt.legend()
+
     if not doplot:
-        pl.close('all')
-        
+        plt.close('all')
+
     return artists
 
 
@@ -75,21 +76,21 @@ def test_other(doplot=doplot):
     nrows2,ncols2 = sc.getrowscols(1)
     assert (nrows,ncols) == (8,13)
     assert (nrows2,ncols2) == (1,1) # Check that it works with a single row
-    
-    sc.emptyfig()
-    o.fig = pl.figure('Limits')
 
-    pl.subplot(2,1,1)
-    pl.plot(data)
+    sc.emptyfig()
+    o.fig = plt.figure('Limits')
+
+    plt.subplot(2,1,1)
+    plt.plot(data)
     sc.boxoff()
     sc.setxlim()
     sc.setylim()
     sc.commaticks()
 
-    pl.subplot(2,1,2)
-    pl.plot(data)
+    plt.subplot(2,1,2)
+    plt.plot(data)
     sc.SIticks()
-    pl.title('SI ticks')
+    plt.title('SI ticks')
 
     try:
         sc.maximize()
@@ -102,22 +103,22 @@ def test_other(doplot=doplot):
     sc.figlayout(keep=True)
 
     # Test legends
-    pl.figure('Legends')
-    pl.plot([1,4,3], label='A')
-    pl.plot([5,7,8], label='B')
-    pl.plot([2,5,2], label='C')
+    plt.figure('Legends')
+    plt.plot([1,4,3], label='A')
+    plt.plot([5,7,8], label='B')
+    plt.plot([2,5,2], label='C')
     sc.orderlegend(reverse=True) # Legend order C, B, A
     sc.orderlegend([1,0,2], frameon=False) # Legend order B, A, C with no frame
     sc.separatelegend()
-    
+
     # Test styles
     style = 'sciris.fancy' if sc.compareversions(mpl, '>=3.7') else sc.style_fancy # Matplotlib regression support
-    with pl.style.context(style):
-        pl.figure('Custom style')
-        pl.plot(pl.rand(10))
+    with plt.style.context(style):
+        plt.figure('Custom style')
+        plt.plot(np.random.rand(10))
 
     if not doplot:
-        pl.close('all')
+        plt.close('all')
 
     return o
 
@@ -132,21 +133,21 @@ def test_saving(doplot=doplot):
     fn.anim  = 'testanim.gif' # mp4 only available if ffmpeg is installed
 
     print('Testing save figs')
-    o.fig = pl.figure('Save figs')
-    pl.plot(pl.rand(10))
+    o.fig = plt.figure('Save figs')
+    plt.plot(np.random.rand(10))
 
     sc.savefigs(o.fig, filetype='fig', filename=fn.fig)
     sc.loadfig(fn.fig)
 
     print('Testing save movie')
-    frames = [pl.plot(pl.cumsum(pl.randn(100))) for i in range(2)] # Create frames
+    frames = [plt.plot(np.cumsum(np.random.randn(100))) for i in range(2)] # Create frames
     sc.savemovie(frames, fn.movie) # Save movie as medium-quality gif
 
     print('Testing animation')
     anim = sc.animation(filename=fn.anim)
-    pl.figure('Animation')
+    plt.figure('Animation')
     for i in range(2):
-        pl.plot(pl.cumsum(pl.randn(100)))
+        plt.plot(np.cumsum(np.random.randn(100)))
         anim.addframe()
     anim.save()
 
@@ -155,7 +156,7 @@ def test_saving(doplot=doplot):
         os.remove(f)
 
     if not doplot:
-        pl.close('all')
+        plt.close('all')
 
     return o
 
@@ -165,21 +166,49 @@ def test_dates(doplot=doplot):
     o = sc.objdict()
 
     x = np.array(sc.daterange('2020-12-24', '2021-01-15', asdate=True))
-    y = sc.smooth(pl.rand(len(x)))
-    o.fig = pl.figure('Date formatters', figsize=(6,6))
+    y = sc.smooth(np.random.rand(len(x)))
+    o.fig = plt.figure('Date formatters', figsize=(6,6))
 
     for i,style in enumerate(['auto', 'concise', 'sciris']):
-        pl.subplot(3,1,i+1)
-        pl.plot(x, y)
-        pl.title('Date formatter: ' + style.title())
+        plt.subplot(3,1,i+1)
+        plt.plot(x, y)
+        plt.title('Date formatter: ' + style.title())
         sc.dateformatter(style=style, rotation=1)
 
-    pl.figure('Datenum formatter')
-    pl.plot(np.arange(500), pl.randn(500))
+    o.fig.tight_layout()
+
+    plt.figure('Datenum formatter')
+    plt.plot(np.arange(500), np.random.randn(500))
     sc.datenumformatter(start_date='2021-01-01')
 
+    # Plot more use cases
+    x1 = np.arange(1990, 2021) # Numerical years
+    x2 = np.linspace(2020, 2022) # Fractional years
+    x3 = pd.date_range('2020-01-01', periods=24, freq='M') # 3. pd.Timestamp objects
+    x4 = np.array(x3, dtype='datetime64') # 4. numpy.datetime64 objects
+    x5 = [(str(year)+'-01-01') for year in range(1990, 2021)] # 5. strings
+
+    x_vals = [x1, x2, x3, x4]
+    titles = ['Integer years', 'Fractional years', 'pd.Timestamp', 'np.datetime64']
+
+    o.fig2 = plt.figure('More dateformatter formats', figsize=(10, 12))
+    # axs = plt.subplots()
+    for i, (x, title) in enumerate(zip(x_vals, titles)):
+        ax = plt.subplot(5, 1, i+1)
+        y = np.random.rand(len(x))
+        ax.plot(x, y, marker='o')
+        ax.set_title(title)
+        sc.dateformatter(ax)
+
+    with pytest.warns(RuntimeWarning):
+        ax = plt.subplot(5,1,5)
+        ax.plot(x5, np.random.rand(len(x5)))
+        sc.dateformatter(ax)
+
+    o.fig2.tight_layout()
+
     if not doplot:
-        pl.close('all')
+        plt.close('all')
 
     return o
 
@@ -191,18 +220,18 @@ def test_fonts(doplot=doplot):
     fonts = sc.fonts()
 
     # Test setting fonts
-    orig = pl.rcParams['font.family']
+    orig = plt.rcParams['font.family']
     sc.fonts(add=sc.path(sc.thispath() / 'files/examplefont.ttf'), use=True, die=True, verbose=True)
 
-    pl.figure('Fonts')
-    pl.plot([1,2,3], [4,5,6])
-    pl.xlabel('Example label in new font')
+    plt.figure('Fonts')
+    plt.plot([1,2,3], [4,5,6])
+    plt.xlabel('Example label in new font')
 
     # Reset
-    pl.rcParams['font.family'] = orig
+    plt.rcParams['font.family'] = orig
 
     if not doplot:
-        pl.close('all')
+        plt.close('all')
 
     return fonts
 
@@ -210,8 +239,8 @@ def test_fonts(doplot=doplot):
 def test_saveload(doplot=doplot):
     sc.heading('Testing figure save/load')
 
-    fig = pl.figure('Save/load')
-    pl.plot([1,3,7])
+    fig = plt.figure('Save/load')
+    plt.plot([1,3,7])
     fn = sc.objdict()
     fn.png1a = 'example1.png'
     fn.png1b = os.path.abspath('example1.png')
@@ -250,7 +279,7 @@ def test_saveload(doplot=doplot):
             print(f'Could not remove {f}: {E}')
 
     if not doplot:
-        pl.close('all')
+        plt.close('all')
 
     return md2
 
@@ -262,16 +291,16 @@ if __name__ == '__main__':
     doplot = True
     sc.options(interactive=True)
 
-    threed    = test_3d(doplot)
-    artists   = test_stackedbar(doplot)
-    other     = test_other(doplot)
-    saved     = test_saving(doplot)
+    # threed    = test_3d(doplot)
+    # artists   = test_stackedbar(doplot)
+    # other     = test_other(doplot)
+    # saved     = test_saving(doplot)
     dates     = test_dates(doplot)
-    fonts     = test_fonts(doplot)
-    metadata  = test_saveload(doplot)
+    # fonts     = test_fonts(doplot)
+    # metadata  = test_saveload(doplot)
 
     if doplot:
-        pl.show()
+        plt.show()
 
     T.toc()
     print('Done.')
